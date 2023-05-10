1a86e2e91179f6dd4aa26dfede56191fa22942f9
git <sara.bernini@stud.unifi.it>
2023-05-10 14:26:54 +0200

    inserimento della stringa con il messaggio in arrivo nella classe messaggio. collegamento a db.

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -1,5 +1,6 @@
 package org.example;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
@@ -14,6 +15,7 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -21,11 +23,14 @@ import java.util.Properties;
 public class Consumer {
     Schema schema ;
     DatumReader<GenericRecord> datumReader;
+    ArrayList<Message> messageList;
 
 
-    public Consumer() throws IOException {
+
+    public Consumer(ArrayList<Message> messageList) throws IOException {
         this.schema = new Schema.Parser().parse(new File("src/main/java/org/example/avroschema.avsc"));
         this.datumReader = new GenericDatumReader<>(schema);
+        this.messageList = messageList;
     }
 
     //metodo che legge i dati dal sensore
@@ -59,10 +64,40 @@ public class Consumer {
             consumer.close();
         }
     }
-    //metodo che deserializza i dati ricevuti
+    //metodo che deserializza i dati ricevuti, li traduce in un oggetto messaggio che memorizza in una lista di messaggi
     public void deserialize(ConsumerRecord<String, byte[]> record) throws IOException {
         Decoder decoder = DecoderFactory.get().binaryDecoder(record.value(), null);
-        System.out.println(this.datumReader.read(null, decoder));
+        GenericRecord r= this.datumReader.read(null, decoder);
+
+        Message.MessageType message_type = Message.MessageType.valueOf(r.get(0).toString());
+        int message_id =Integer.valueOf(r.get(1).toString());
+        String station_name =String.valueOf(r.get(2));
+        String timestamp=String.valueOf(r.get(3));
+        String acquisition_timestamp=String.valueOf(r.get(4));
+        String gps_timestamp=String.valueOf(r.get(5));
+        float latitude= Float.valueOf(r.get(6).toString());
+        float longitude= Float.valueOf(r.get(7).toString());
+        ArrayList<Value> values= new ArrayList<>();
+        GenericData.Array v= (GenericData.Array) r.get("values");
+        for(int i=0; i<v.size(); i++ ){
+            Value value= new Value();
+            GenericRecord sensor= (GenericRecord) v.get(i);
+            value.setValue(Double.parseDouble(sensor.get(0).toString()));
+            value.setSensor_name(String.valueOf(sensor.get(1)));
+            values.add(value);
+        }
+        ArrayList<ModelValues> modelValues= new ArrayList<>();
+        GenericData.Array mv= (GenericData.Array) r.get(9);
+        for(int i=0; i<mv.size(); i++ ){
+            GenericRecord model= (GenericRecord) mv.get(i);
+            ModelValues modelValue = new ModelValues();
+            modelValue.setPosition(Integer.valueOf(model.get(0).toString()));
+            modelValue.setSensor_name(String.valueOf(model.get(1)));
+        }
+        String command = String.valueOf(r.get(10));
+        Message message= new Message(message_type,message_id,station_name,timestamp,acquisition_timestamp,gps_timestamp,latitude,longitude,values,modelValues,command);
+        messageList.add(message);
     }
+
 }
 
--- src/main/java/org/example/Message.java
+++ src/main/java/org/example/Message.java
@@ -1,27 +1,59 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Message {
+    public Message(MessageType message_type, int message_id, String station_name, String timestamp, String acquisition_timestamp, String gps_timestamp, float latitude, float longitude, ArrayList<Value> values, ArrayList<ModelValues> model, String command) {
+        this.message_type = message_type;
+        this.message_id = message_id;
+        this.station_name = station_name;
+        this.timestamp = timestamp;
+        this.acquisition_timestamp = acquisition_timestamp;
+        this.gps_timestamp = gps_timestamp;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.values = values;
+        this.model = model;
+        this.command = command;
+    }
+
     enum MessageType {DATA, STATUS, CONTROL, MODEL, EXIT}
 
     private MessageType message_type;
-    private int message_id;
-    private String station_name;
-    private String timestamp;
-    private String acquisition_timestamp;
-    private String gps_timestamp;
+    private int message_id = 0;
+    private String station_name ="";
+    private String timestamp="";
+    private String acquisition_timestamp="";
+    private String gps_timestamp="";
     private float latitude;
     private float longitude;
-    private Value[] values;
-    private ModelValues[] model;
-    private String command;
+    private ArrayList<Value> values;
+    private ArrayList<ModelValues> model;
+    private String command = "";
 }
 
 class Value {
     private double value;
     private String sensor_name;
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public void setSensor_name(String sensor_name) {
+        this.sensor_name = sensor_name;
+    }
 }
 
 class ModelValues {
     private int position;
     private String sensor_name;
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setSensor_name(String sensor_name) {
+        this.sensor_name = sensor_name;
+    }
 }
